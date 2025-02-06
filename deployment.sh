4f26b751c60809c2afa793e49c6d482a4d1e43a6
Erik William Tahara Gonçalves <erikwtg@gmail.com>
2025-02-05 22:45:54 -0300

    Äjustado nome da rede configurada no script para rodar o projeto

--- deployment.sh
+++ deployment.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-if docker network inspect garupa-bank-network &>/dev/null; then
-  echo "Network garupa-bank-network already exists."
+if docker network inspect garupa_bank-network &>/dev/null; then
+  echo "Network garupa_bank-network already exists."
 else
-  docker network create garupa-bank-network
-  echo "Network garupa-bank-network created."
+  docker network create garupa_bank-network
+  echo "Network garupa_bank-network created."
 fi
 
 docker-compose -f docker-compose.yml up -d --build
@@ -12,7 +12,7 @@ docker-compose -f docker-compose.yml up -d --build
 echo "Containers started successfully."
 
 # Verifica se o container está rodando
-if docker ps | grep -q "garupa-bank-network"; then
+if docker ps | grep -q "garupa_bank-network"; then
   echo "Container is running."
 else
   echo "Container is not running."  
